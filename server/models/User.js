3d8e8c1470943f51fa778a45edb63f388b66a6b9
Eric-0517 <0147zeo@gmail.com>
2025-11-21 22:40:59 +0800

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,21 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   discordId: { type: String, required: true, unique: true },
-  username: String,
-  discriminator: String,
-  avatar: String,
-  role: { type: String, default: "user" },
+  username: { type: String, required: true },       // Discord 名稱
+  nickname: { type: String },                        // 遊戲暱稱
+  rank: { type: String },                            // 遊戲排位
+  realname: { type: String },
+  phone: { type: String },
+  email: { type: String },
+  birthday: { type: Date },
+  taiwan: { type: String },
+  idNumber: { type: String },
+}, { timestamps: true });
 
-  // 個人資料欄位
-  nickname: String,
-  rank: String,
-  realname: String,
-  phone: String,
-  email: String,
-  birthday: String,
-  taiwan: String,
-  id: String
-});
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", userSchema);
